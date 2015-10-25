58e9d340ffa9e7b01123bb0997faf92fe83d0541
CreativeSeo33 <alexeyfedotof@gmail.com>
2015-10-25 21:49:37 +0300

    new

--- dev/blocks/footer.php
+++ dev/blocks/footer.php
@@ -1,33 +1,18 @@
 <footer class="footer">
-	<div class="container-fluid">
+	<div class="container">
 		<div class="row">
-			
-			  <div class="col-md-12">
-			  	<div class="footer_wrap">
-			  		
-				    <div class="row">
-				      <div class="col-md-9">
-				      	<div class="footer_links">
-				      		<div class="row">
-				      			<div class="col-md-4">
-				      				<div class="footer_links_h3">Продвижение сайтов</div>
-				      			</div>
-				      			<div class="col-md-4">
-				      				<div class="footer_links_h3">Создание сайтов</div>
-				      			</div>
-				      			<div class="col-md-4">
-				      				<div class="footer_links_h3">Контекстная реклама</div>
-				      			</div>
-				      		</div>
-				      	</div>				        
-				      </div>
-				      <div class="col-md-3">
-				        <div class="footer_links_h3">Контакты</div>				       
-				      </div>
-				    </div>
-			  	</div>			    
-			  </div>
-			
+			<div class="col-md-3">
+				<div class="footer_links_h3">Продвижение сайтов</div>
+		 	</div>
+			<div class="col-md-3">
+				<div class="footer_links_h3">Создание сайтов</div>
+			</div>
+			<div class="col-md-3">
+				<div class="footer_links_h3">Контекстная реклама</div>
+			</div>     					      			        
+			<div class="col-md-3">
+				<div class="footer_links_h3">Контакты</div>				       
+			</div>
 		</div>
 	</div>
 </footer>
--- dev/blocks/navbar.php
+++ dev/blocks/navbar.php
@@ -7,21 +7,32 @@
 				</div>
 			</div>
 			<div class="col-md-6">
-				<ul class="center-block">
+				
+				<ul class="pull-right">
 					<li><a href="">Продвижение</a></li>
 					<li><a href="">Создание</a></li>
 					<li><a href="">Наша команда</a></li>
 					<li><a href="">Портфолио</a></li>
 					<li><a href="">Контакты</a></li>
 				</ul>
+				
 		 	</div>
 			<div class="col-md-2">
 				<div class="nav_phone">+7 930 830-03-12</div>
 			</div>
-			<div class="col-md-2 nav_menu"><span><a href="" class="tIcons_button">МЕНЮ</a></span>
+			<div class="col-md-2 nav_menu">
+			  <div class="pull-right">
+				<span>
+					<a href="" class="tIcons_button">МЕНЮ</a>
+				</span>
 				<a class="tIcons" href="#">
 				    <!-- Gallery Icon -->
 				    <svg class="mi" viewBox="0 0 36 30">
+				      <style>
+				      	.mi rect {
+				      		fill:#333;
+				      	}
+				      </style>
 				      <rect x="3" width="8" height="8"/>
 				      <rect x="14" width="8" height="8"/>
 				      <rect x="25" width="8" height="8"/>
@@ -37,6 +48,7 @@
 				      <polygon points="32.8,4.4 28.6,0.2 18,10.8 7.4,0.2 3.2,4.4 13.8,15 3.2,25.6 7.4,29.8 18,19.2 28.6,29.8 32.8,25.6 22.2,15 "/>
 				    </svg>				  
 				</a>
+			  </div>
 			</div>
 		</div>
 	</div>
