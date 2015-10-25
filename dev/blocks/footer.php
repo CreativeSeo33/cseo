1f2a9e0fc2bac9504b182033f7a512934e65bf63
CreativeSeo33 <alexeyfedotof@gmail.com>
2015-10-25 23:49:59 +0300

    new

--- dev/blocks/footer.php
+++ dev/blocks/footer.php
@@ -2,18 +2,66 @@
 	<div class="container">
 		<div class="row">
 			<div class="col-md-3">
-				<div class="footer_phone">+7 (4922) 600-312</div>
-				<div class="footer_phone">+7 (930) 830-03-12</div>
+				<div class="footer_phone">+7 4922 600-312</div>
+				<div class="footer_phone">+7 930 830-03-12</div>
+				<div class="footer_time">c 9 до 18 ПН - ПТ</div>
+				<div class="footer_time">600001, г. Владимир <br> ул. Дворянская 27а, корпус 1</div>
 		 	</div>
 			<div class="col-md-3">
-				<div class="footer_h3">Создание сайтов</div>
+				<div class="footer_h3"><a href="">Продвижение сайтов</a></div>
+				<ul>
+					<li><a href="">Продвижение интернет-магазина</a></li>
+					<li><a href="">Продвижение нового сайта</a></li>
+					<li><a href="">Seo аудит</a></li>
+					<li><a href=""></a></li>
+					<li><a href=""></a></li>
+				</ul>
 			</div>
 			<div class="col-md-3">
-				<div class="footer_h3">Контекстная реклама</div>
+				<div class="footer_h3"><a href="">Создание сайтов</a></div>
+				<ul>
+					<li><a href="">Интернет-магазин</a></li>
+					<li><a href="">Корпоративный сайт</a></li>
+					<li><a href="">Сайт визитка</a></li>
+					<li><a href="">Создание контента</a></li>
+					<li><a href=""></a></li>
+				</ul>
 			</div>     					      			        
 			<div class="col-md-3">
-				<div class="footer_h3">Контакты</div>				       
+				<div class="footer_h3"><a href="">Контекстная реклама</a></div>
+				<ul>
+					<li><a href="">Яндекс.Директ</a></li>
+					<li><a href="">Google Adwords</a></li>					
+				</ul>				       
 			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-3">
+
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<li><a href="">Телефония</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<li><a href="">Портфолио</a></li>
+					<li><a href="">Прайс-лист</a></li>
+				</ul>
+			</div>
+			<div class="col-md-3">
+				<ul>
+					<li><a href="">Реквизиты</a></li>
+					<li><a href="">Вакансии</a></li>
+					<li><a href="">Блог</a></li>					
+				</ul>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-md-12">
+				<div class="footer_copyright">© 2015, CreativeSeo</div>
+			</div>			
 		</div>
 	</div>
 </footer>
--- dev/contacts/index.php
+++ dev/contacts/index.php
@@ -14,42 +14,18 @@ include("../blocks/navbar.php");
 	<div id="cd-zoom-out"></div>	
 	<div class="contacts_block_map">
 		<h1>Контакты</h1>
-		<table class="contacts_block_map_table">
-		    <tr>
-		        <td>
-		        	<span class="fa-stack fa-lg">
-					  <i class="fa fa-circle fa-stack-2x"></i>
-					  <i class="fa fa-map-marker fa-stack-1x fa-inverse"></i>
-					</span>
-				</td>
-		        <td class="contacts_block_map_text">
-		        	<span>600001, г.Владимир, <br> ул. Дворянская 27а, <br> корпус 1</span>
-		        </td>
-		    </tr>
-		    <tr>
-		        <td>
-		        	<span class="fa-stack fa-lg">
-					  <i class="fa fa-circle fa-stack-2x"></i>
-					  <i class="fa fa-phone fa-stack-1x fa-inverse"></i>
-					</span>
-		        </td>
-		        <td class="contacts_block_map_text">
+		
+		        <div class="contacts_block_map_text">600001, г.Владимир,<br>ул. Дворянская 27а, корпус 1</div>
+		       
+		        <div class="contacts_block_map_text_phone">
 		        	+7 (4922) 600-312 <br>
 					+7 (930) 830-03-12
-		        </td>
-		    </tr>
-		    <tr>
-		        <td>
-		        	<span class="fa-stack fa-lg">
-					  <i class="fa fa-circle fa-stack-2x"></i>
-					  <i class="fa fa-envelope fa-stack-1x fa-inverse"></i>
-					</span>
-		        </td>
-		        <td class="contacts_block_map_text">
+		        </div>
+		   
+		        <div class="contacts_block_map_text">
 		        	<a href="mailto:info@creative-seo.ru.ru">info@creative-seo.ru</a>
-		        </td>
-		    </tr>
-		</table>
+		        </div>
+		  
 	
 	</div>
 </section>
